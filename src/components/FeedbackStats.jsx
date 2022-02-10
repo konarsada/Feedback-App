ff672ae58f7ae01ef255948f07c1e0a0ca564487
konarsada <sadabratak@gmail.com>
2022-02-11 02:01:33 +0530

    Context API and useContext Hook

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import {useContext} from 'react'
 
-function FeedbackStats({feedback}) {
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackStats() {
+    const {feedback} = useContext(FeedbackContext)
+
     let average = feedback.reduce((acc, cur) => {
         return acc + cur.rating
     }, 0) / feedback.length
@@ -14,10 +18,6 @@ function FeedbackStats({feedback}) {
             <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
         </div>
     )
-}
-
-FeedbackStats.propTypes = {
-    feedback: PropTypes.array.isRequired
 }
 
 export default FeedbackStats
